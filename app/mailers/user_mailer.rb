b708fae7f48cf96f5c4f055e3fcc353d163311e4
Clementine <clementine.fouque@gmail.com>
2020-11-24 17:58:15 +0100

    welcome email

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,16 @@
+class UserMailer < ApplicationMailer
+  default from: 'bicravetonchaton@yopmail.com'
+
+  def welcome_email(user)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @user = user
+
+    mail(
+      to: @user.email,
+      subject: 'Miaou à toi !'
+    )
+
+  end
+
+
+end
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module TheKittenProject
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_mailer.delivery_method = :mailjet_api
+
   end
 end
