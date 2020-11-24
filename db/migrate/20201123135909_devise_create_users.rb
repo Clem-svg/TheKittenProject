903b413ba1831020aeac44927fe7e6487961750d
BenjaminServal <benjiserval@gmail.com>
2020-11-24 11:11:07 +0100

    Sign in and Log in

--- db/migrate/20201123135909_devise_create_users.rb
+++ db/migrate/20201123135909_devise_create_users.rb
@@ -3,8 +3,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, null: false, default: ""
+      t.string :last_name,  null: false, default: ""
       
       ## Database authenticatable
       t.string :email,              null: false, default: ""
