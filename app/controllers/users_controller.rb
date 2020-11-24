5da2eeb98223aabc73b09aece45eecd34ef37d23
BenjaminServal <benjiserval@gmail.com>
2020-11-24 12:14:53 +0100

    modif user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,24 @@ class UsersController < ApplicationController
     def create
     end
 
+    def edit
+        @user = current_user
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+          flash[:notice] = "Profil édité !"
+          redirect_to user_path(@user.id)
+        else
+          flash.now[:alert] = "Impossible d'éditer le profil :"
+          render :edit
+        end 
+    end
+
+    private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
+
 end
