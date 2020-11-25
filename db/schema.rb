02b580eaaef1db7a28ea12731d255d0efd5b4421
Aimery Cayol <aimerycayol@gmail.com>
2020-11-25 21:38:37 +0100

    last change

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_132553) do
   end
 
   create_table "cart_lines", force: :cascade do |t|
-    t.integer "quantity"
+    t.integer "quantity", default: 1
     t.bigint "item_id"
     t.bigint "cart_id"
     t.datetime "created_at", null: false
@@ -70,8 +70,8 @@ ActiveRecord::Schema.define(version: 2020_11_24_132553) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
